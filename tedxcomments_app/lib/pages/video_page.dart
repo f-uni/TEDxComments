75c05ba1f11786e5f2cfc83edbd87566b0ab5e85
Filippo Colpani <filippocolpani@gmail.com>
2024-08-08 00:42:30 +0200

    Video player

--- tedxcomments_app/lib/pages/video_page.dart
+++ tedxcomments_app/lib/pages/video_page.dart
@@ -1,41 +1,92 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:tedxcomments_app/models/talk.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class VideoPage extends StatefulWidget {
   const VideoPage({super.key, required this.talk});
   final Talk talk;
+
   @override
   State<VideoPage> createState() => _VideoPageState();
 }
 
 class _VideoPageState extends State<VideoPage> {
   late final Talk talk;
+  late final String videoUrl;
+  late final WebViewController controller;
+  late final Timer timer;
+  int time = 0;
   @override
   void initState() {
     super.initState();
-    talk=widget.talk;
+    talk = widget.talk;
+    videoUrl = talk.url.replaceFirst("https://www", "https://embed");
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) async {
+            controller.runJavaScript(
+                "document.getElementsByClassName('Embeddable__button Embeddable__controls__outbound')[0].style.display='none'");
+            timer = Timer.periodic(
+                const Duration(seconds: 10), (Timer t) => _readTiming());
+          },
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.prevent;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(videoUrl));
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const AspectRatio(
-                aspectRatio: 16 / 9,
-                child: Placeholder()),
-            Text(
-              talk.title,
-              style: const TextStyle(fontSize: 22)
-            ),
-            Text(
-              "${talk.speakers} - ${talk.views} views",
-              style: const TextStyle(fontSize: 14),
-            )
-          ],
-        )
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: WebViewWidget(controller: controller))),
+              Text(talk.title, style: const TextStyle(fontSize: 22)),
+              Text(
+                "${talk.speakers} - ${talk.views} views",
+                style: const TextStyle(fontSize: 14),
+              ),
+              Text("timing = $time ")
+              
+            ],
+          ),
+        ));
+  }
+
+  _readTiming() async {
+    String time = await controller.runJavaScriptReturningResult(
+            "document.getElementsByClassName('Embeddable__controls__current')[0].innerHTML")
+        as String;
+
+    final ms = time.replaceAll("\"", "").split(":");
+    setState(() {
+      this.time = int.parse(ms[0]) * 60 + int.parse(ms[1]);
+    });
   }
 }
